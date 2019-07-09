e5d73cec2012ccdedeff1e2d90303d7da5121cde
Suzanne Smigielski <ssmigiel571@gmail.com>
2019-07-08 22:17:50 -0500

    foundation code

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 $(document).ready(function(){
 
-const searchURL = ' '
+function getUserRepos(){
+let gitAPI = 'https://api.github.com/users/'; 
+let username = `${username}`;
+let apiPath = gitAPI+username+'/repos?type=all&direction=desc';
 
+fetch(apiPath)
+.then(status)
+.then(response => response.json())
+.then(responseJson =>
+    displayResults(responseJson))
 
+}
 
+function status(responseJson){
+    if(!response.ok){
+        throw alert("User not found. Try again.");
+    }
+    return response;
+}
 
+function displayResults(responseJson){
+    $('#results-list').empty();
+    for (let i = 0; i < responseJson ) {
+        $('#results-list').append(
 
-
-
-
-
-
+        )
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+}//jquery doc -end bracket 
