290e541be6dc7326dff42185f9496cef4d343b9b
Suzanne Smigielski <ssmigiel571@gmail.com>
2019-07-08 22:56:41 -0500

    api added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 function getUserRepos(){
 let gitAPI = 'https://api.github.com/users/'; 
-let username = `${username}`;
+let username = $('#username').val();
 let apiPath = gitAPI+username+'/repos?type=all&direction=desc';
 
 fetch(apiPath)
@@ -22,15 +22,26 @@ function status(responseJson){
 
 function displayResults(responseJson){
     $('#results-list').empty();
-    for (let i = 0; i < responseJson ) {
+    for (let i = 0; i < responseJson.length; i++) {
         $('#results-list').append(
-
-        )
-    }
-
-
+            `<h3>Your Results:</h3>
+            <li>
+            <p>${responseJson[i].name}</p>
+            <p><a href="${response.Json[i].html_url}"></a></p>
+            </li>`
+        )};
+    $('#results').removeClass('hidden');
 }
 
+function runForm(){
+    $('form').submit(e=>{
+        event.preventDefault();
+        getUserRepos();
+
+    });
+}
+
+$(runForm);
 
 
 
@@ -39,4 +50,4 @@ function displayResults(responseJson){
 
 
 
-}//jquery doc -end bracket 
+})
